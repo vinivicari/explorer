bd0950fcbab214b9444104c0f2e4f517be4891f1
vinivicari <100092307+vinivicari@users.noreply.github.com>
2023-08-17 02:00:56 -0400

    Add files via upload

---
+++ STAGE5/focustimer/src/FocusTimer/actions.js
@@ -0,0 +1,34 @@
+import state from './state.js'
+import * as timer from './timer.js'
+import * as el from './elements.js'
+import * as sounds from './sounds.js'
+
+export function toggleRunning(){
+  state.isRunning = document.documentElement.classList.toggle('running')
+  
+  timer.countdown()
+  sounds.buttonPressAudio.play()
+}
+
+export function reset(){
+  state.isRunning = false
+  document.documentElement.classList.remove('running')
+  timer.updateDisplay()
+}
+
+export function set(){
+  el.minutes.setAttribute('contenteditable', true)
+  el.minutes.focus()
+}
+
+export function toggleMusic() {
+  state.isMute = document.documentElement.classList.toggle('music-on')
+  if(state.isMute){
+    sounds.bgAudio.play()
+  } else {
+    sounds.bgAudio.pause()
+  }
+
+}
+
+
